75b436bd018e741534f221c5a2c9c8f2844762f3
FiveEYZ <b-d-t@msn.com>
2016-10-18 13:27:04 +0200

    Revert "Added stuff"
    This reverts commit 6e77be6f07badaa9e5a0155003ebb49fe56a5cc0.

--- OOPlab/Form1.cs
+++ OOPlab/Form1.cs
@@ -16,24 +16,5 @@ namespace OOPlab
         {
             InitializeComponent();
         }
-
-        private void Label1_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Label1.Text = "Hello World";
-        }
-
-        private void button2_MouseClick(object sender, MouseEventArgs e)
-        {
-            Label1.Text = "";
-        }
-
-        private void button3_MouseClick(object sender, MouseEventArgs e)
-        {
-            Application.Exit();
-        }
     }
 }
